69dacbc4419b78ea05a44603ae94165119dc470c
Sandeep Erukulla <sandeep.erukulla2706@gmail.com>
2025-08-23 17:07:06 +0530

    Initial commit for Trade Compliance - Spring Boot project setup

---
+++ src/main/java/io/github/erukullasandeep/trade_compliance_service/TradeComplianceServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.erukullasandeep.trade_compliance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeComplianceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeComplianceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/erukullasandeep/trade_compliance_service/TradeComplianceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.erukullasandeep.trade_compliance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeComplianceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
